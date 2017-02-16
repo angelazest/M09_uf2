722beeb7feb2fc2183aa507f68780532d8ce7f81
angela <angelazest@gmail.com>
2017-02-16 22:36:54 +0100

    Primera versio app. Codi del gestor de comptes bancari.

---
+++ src/activitat_5/Main.java
@@ -0,0 +1,18 @@
+package activitat_5;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+	   //double valorActual;
+	   //double valorAnterior;		
+        
+        miCuenta = new CCuenta("Juan López","1000-2365-85-123456789",2500,0);
+       
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+    }
+
+}
